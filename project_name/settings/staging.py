12d87d294e9e326f01412bb380c16df7fc72e1ff
Pablo García <poli@devartis.com>
2016-11-17 20:58:47 -0500

    Update staging.py
    Database host setting

--- project_name/settings/staging.py
+++ project_name/settings/staging.py
@@ -15,6 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.environ['DATABASE_HOST'],
         'NAME': os.environ['DATABASE_NAME'],
         'USER': os.environ['DATABASE_USER'],
         'PASSWORD': os.environ['DATABASE_PASSWORD'],

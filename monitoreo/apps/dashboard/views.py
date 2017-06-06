63f518575fe5489af4729265d649ba72026a250b
Lucas Lavandeira <lucaslavandeira@gmail.com>
2017-06-06 15:54:40 -0300

    Reemplazo más valores hardcodeados por indicadores obtenidos del backend

--- monitoreo/apps/dashboard/views.py
+++ monitoreo/apps/dashboard/views.py
@@ -5,13 +5,23 @@ from .models import IndicadorRed
 
 
 def landing(request):
-    indicators = IndicadorRed.objects.filter(fecha=date.today())
+    today = date.today()
+    indicators = IndicadorRed.objects.filter(fecha=today)
 
     # Si todavía no se calcularon los indicadores de hoy (12 AM - 5 AM) usamos
     # los de ayer
     if not indicators:
-        yesterday = date.today() - timedelta(days=1)
+        yesterday = today - timedelta(days=1)
         indicators = IndicadorRed.objects.filter(fecha=yesterday)
+
+    catalogos_cant = indicators.get(
+        indicador_nombre="catalogos_cant").indicador_valor
+
+    datasets_cant = indicators.get(
+        indicador_nombre="datasets_cant").indicador_valor
+
+    items = 0
+    jurisdicciones = 0
 
     ok_pct = indicators.get(
         indicador_nombre="datasets_meta_ok_pct").indicador_valor
@@ -19,7 +29,17 @@ def landing(request):
     actualizados_pct = indicators.get(
         indicador_nombre="datasets_actualizados_pct").indicador_valor
 
+    documentados_pct = 60
+    descargables_pct = 75
+
     context = {
+        'fecha': today,
+        'items': items,
+        'jurisdicciones': jurisdicciones,
+        'catalogos': catalogos_cant,
+        'datasets': datasets_cant,
+        'documentados_pct': documentados_pct,
+        'descargables_pct': descargables_pct,
         'ok_pct': ok_pct,
         'actualizados_pct': actualizados_pct
     }
--- monitoreo/static/js/monitoreo/landing.js
+++ monitoreo/static/js/monitoreo/landing.js
@@ -1,29 +1,29 @@
 var ProgressBar;
 
-function chart(id, pct, step_fn) {
+function chart(id, step_fn) {
+    var pct = $(id).attr("data-pct")/100;
     var settings = {
         strokeWidth: 2,
-        duration: 1400,
+        duration: 1400 * pct,
         easing: 'easeInOut'
     };
     settings.step = step_fn ? step_fn : default_value;
     var circle = new ProgressBar.Circle(id, settings);
+
+    // Leo el porcentaje de el campo 'data-pct' del div. Obligatorio!
     circle.animate(pct);
     return circle;
 }
 
 window.onload = function() {
-    chart("#donut-metadatos", $("#donut-metadatos").attr("data-pct")/100);
-    chart("#donut-actualizados", $("#donut-actualizados").attr("data-pct")/100);
-    var circle = chart("#donut-documentados", 0.5);
-    chart("#donut-descargables", 0.5);
+    chart("#donut-metadatos");
+    chart("#donut-actualizados");
+    chart("#donut-documentados");
+    chart("#donut-descargables");
 };
 
-function empty() {
-
-}
-
 function default_value(_, circle) {
+    // Setea el texto al valor porcentual del progreso en cada paso
     var value = Math.round(circle.value() * 100);
     if (value === 0) {
       circle.setText('');

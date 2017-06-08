bfe7fbe392d2a39d627a05ed78f9fbcaab93b725
Lucas Lavandeira <lucaslavandeira@gmail.com>
2017-06-08 15:45:19 -0300

    Agrego vista básica de tabla de indicadores del PAD

--- monitoreo/apps/red_nodos/views.py
+++ monitoreo/apps/red_nodos/views.py
@@ -1,6 +1,34 @@
+from datetime import date, timedelta
 from django.shortcuts import render
 from monitoreo.apps.dashboard.models import Indicador
 
 
 def red_nodos(request):
-    return render(request, 'nodos/nodos.html')
+    today = date.today()
+    indicadores = Indicador.objects.filter(fecha=today)
+    if not indicadores:
+        yesterday = today - timedelta(days=1)
+        indicadores = Indicador.objects.filter(fecha=yesterday)
+
+    if not indicadores:  # Error, no hay indicadores cargados
+        return render(request, '500.html')
+
+    # El template espera una lista de los indicadores en orden correcto
+    catalogs = {}
+    indicators_names = ['datasets_actualizados_pct', 'datasets_meta_ok_pct']
+    for indicador in indicadores:
+        nombre = indicador.catalogo_nombre
+
+        if nombre not in catalogs:
+            catalogs[nombre] = []
+
+        if indicador.indicador_nombre in indicators_names:
+            index = indicators_names.index(indicador.indicador_nombre)
+            catalogs[nombre].insert(index, indicador.indicador_valor)
+
+    context = {
+        'indicator_names': indicators_names,
+        'catalogs': catalogs
+    }
+
+    return render(request, 'nodos/nodos.html', context)

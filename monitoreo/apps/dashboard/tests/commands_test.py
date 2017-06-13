3be652a27a738c458018200d216485acda360148
Lucas Lavandeira <lucaslavandeira@gmail.com>
2017-06-13 14:38:14 -0300

    Agrego test de ejemplo, y edito requirements para poder correrlo

--- monitoreo/apps/dashboard/tests/commands_test.py
+++ monitoreo/apps/dashboard/tests/commands_test.py
@@ -1,11 +1,9 @@
 #! coding: utf-8
 from django.test import TestCase
-from nose.tools import *
-
 from monitoreo.apps.dashboard.models import Indicador
 
 
-class DocumentTests(TestCase):
-    @istest
+class CommandTest(TestCase):
     def test_document_short_title(self):
-        assert_equals(True, True)
+        Indicador.objects.filter(indicador_valor=1)
+        self.assertEqual(True, True)

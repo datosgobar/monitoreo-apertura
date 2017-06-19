1a727bea88168e988d8394c0529b21872e31e8e6
Lucas Lavandeira <lucaslavandeira@gmail.com>
2017-06-19 09:59:07 -0300

    Agrego Makefile para instalación del cron

--- install_cron.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "ROOTDIR=$PWD" >> .cronfile
-echo "PYTHON=$PWD/../venv/bin/python" >> .cronfile
-cat cron_jobs >> .cronfile
-crontab .cronfile
-rm .cronfile
-touch cron_jobs

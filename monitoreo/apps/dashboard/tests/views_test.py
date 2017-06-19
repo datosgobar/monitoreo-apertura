32cc148b01454c4c7f09ca97b2eadd3d5c43e6be
Lucas Lavandeira <lucaslavandeira@gmail.com>
2017-06-19 12:58:15 -0300

    Agrego test simple de views de landing

---
+++ monitoreo/apps/dashboard/tests/views_test.py
@@ -0,0 +1,11 @@
+#! coding: utf-8
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from monitoreo.apps.dashboard.views import landing, compromisos, red_nodos
+
+
+class ViewsTest(TestCase):
+    def test_landing_with_no_indicators_loaded_returns_500(self):
+        response = Client().get(reverse('dashboard:landing'))
+        self.assertEqual(response.status_code, 500)

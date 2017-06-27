4be1d6abff5067ea2ea3997d3b043c0bfa07c0ad
Lucas Lavandeira <lucaslavandeira@gmail.com>
2017-06-27 11:28:44 -0300

    Borro vieja carpeta de deployment

--- deployment/configure_cd_staging.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "Must configure testing deployment for Continuous Delivery"
-echo "See 'deployment/azure.sh' or 'deployment/heroku.sh' or add your own deploy script (or project)"
-exit 0;

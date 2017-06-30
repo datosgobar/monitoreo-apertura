f1048c9825a8168d9fb2ea375187f50ae6fbde9c
Lucas Lavandeira <lucaslavandeira@gmail.com>
2017-06-30 12:13:22 -0300

    Agrego cálculo de indicadores de documentación

--- indicadores_pad/indicators.py
+++ indicadores_pad/indicators.py
@@ -1,7 +1,7 @@
 #! coding: utf-8
 
-import reader
 from pydatajson import DataJson
+from .reader import SpreadsheetReader
 
 # Valores numéricos de las filas en la hoja de distribuciones del PAD
 COMPROMISO = 1
@@ -11,59 +11,92 @@ DISTRIBUCION = 13
 
 
 class PADIndicators:
-    def __init__(self):
-        self.dj = DataJson()
-
-    @staticmethod
-    def count_rows(spreadsheet_id, sheet):
-        # Trae la sheet entera
-        rows = reader.read_sheet(spreadsheet_id)
-        return len(rows)
+    def __init__(self, reader=SpreadsheetReader, data_json=DataJson):
+        self.reader = reader()
+        self.data_json = data_json()
+        self.datajson_cache = {}
 
     def generate_pad_indicators(self, spreadsheet_id):
         """Genera indicadores del PAD, con varios datos sobre el estado de
         las distribuciones y los compromisos.
-        
+
         Args:
-            spreadsheet_id (str): ID de la hoja de datos del PAD sobre la 
+            spreadsheet_id (str): ID de la hoja de datos del PAD sobre la
             cual leer y calcular los indicadores.
-        
+
         Returns:
             dict: diccionario con los indicadores como claves
         """
 
-        rows = reader.read_sheet(spreadsheet_id)
+        sheet = self.reader.read_sheet(spreadsheet_id)
+        indicators = {}
+        indicators.update(self.generate_documentation_indicators(sheet))
+        return indicators
 
     def generate_documentation_indicators(self, sheet):
-        """Genera los indicadores de documentación. Un compromiso es 
+        """Genera los indicadores de documentación. Un compromiso es
         considerado como documentado cuando:
             1. todas sus distribuciones tienen asociado un data.json
-            2. los datasets y distribuciones asociadas al compromiso existen 
+            2. los datasets y distribuciones asociadas al compromiso existen
             dentro de ese data.json
-            3. la metadata de los datasets del compromiso en el data.json pasan 
+            3. la metadata de los datasets del compromiso en el data.json pasan
             la validación de la librería pydatajson
         Args:
-            sheet (str o list): ID de un spreadsheet de google docs o una 
-                lista de filas de una spreadsheet ya parseada
+            sheet (list): lista de dicts de una spreadsheet ya parseada
         Returns:
             dict: diccionario con indicadores de compromisos documentados
                 (cantidad y porcentaje)
         """
+        count = 0
+        documented = 0
+        for compromiso in sheet:
+            count += 1
+            if self.compromiso_is_documented(compromiso):
+                documented += 1
 
-        if isinstance(sheet, str):
-            sheet = reader.get_sheet(sheet, "pad_distribuciones")
+        documented_pct = round(float(documented) / count, 2) * 100
 
-        # compromisos: claves los IDs de cada compromiso, valores una lista
-        # de los data.json asociados a sus distribuciones
-        compromisos = {}
-        for row in sheet:
-            compromiso_id = row[COMPROMISO]
-            if compromiso_id not in compromisos:
-                compromisos[compromiso_id] = []
+        documented_indicators = {
+            'pad_items_documentados_cant': documented,
+            'pad_items_no_documentados_cant': count - documented,
+            'pad_items_documentados_pct': documented_pct
+        }
+        return documented_indicators
 
-            if row[DATAJSON] not in compromisos[compromiso_id]:
-                compromisos[compromiso_id].append(row[DATAJSON])
+    def compromiso_is_documented(self, compromiso):
+        """Verifica si un compromiso está documentado. Un compromiso se
+        considera documentado si:
+            1. Para cada dataset, su campo 'catalog_datajson_url' apunta a un
+            data.json válido
+            2. Todos sus datasets están dentro del data.json asociado
+            3. Los datasets dentro del data.json pasan la validación de
+            'pydatajson'
 
-            ## Incomplete
-            if row[DATASET] not in compromisos[compromiso_id]:
-                pass
+        Args:
+            compromiso (dict): compromiso a validar
+
+        Returns:
+            bool: True si el compromiso está documentado, False caso contrario
+        """
+        for dataset in compromiso['dataset']:
+            datajson_url = dataset.get('catalog_datajson_url')
+            if not datajson_url:  # Falta un datajson, no está documentado
+                return False
+
+            if datajson_url not in self.datajson_cache:
+                self.datajson_cache[datajson_url] = \
+                    self.data_json.validate_catalog(datajson_url)
+
+            validation = self.datajson_cache[datajson_url]['error']['dataset']
+            dataset_title = dataset['dataset_title']
+            valid = False
+            for datajson_dataset in validation:
+                if datajson_dataset['title'] == dataset_title:
+                    if datajson_dataset['status'] != 'OK':
+                        return False
+                    valid = True
+
+            if not valid:
+                return False
+
+        return True
--- indicadores_pad/tests/test_core.py
+++ indicadores_pad/tests/test_core.py
@@ -1,1 +1,180 @@
 #! coding: utf-8
+
+import os
+import unittest
+from indicadores_pad.indicators import PADIndicators
+
+
+class DocumentationTest(unittest.TestCase):
+    """Tests para la clase PADIndicators. Se aprovecha que si se le pasa al
+        reader un diccionario de Python en vez de un ID a un spreadsheet, te
+        devuelve el mismo diccionario para poder testear solo la funcionalidad
+        de cálculo de indicadores.
+    Los compromisos reales obtenidos de la panilla de cálculo del PAD no
+        tienen rutas a los catálogos .json sino URLs. La librería usada para
+        procesarlos debe poder leerlos de ambas fuentes indistintamente.
+    """
+
+    SAMPLES_DIR = os.path.join('tests', 'samples')
+
+    @classmethod
+    def setUpClass(cls):
+        cls.pad = PADIndicators()
+
+    def test_documentation_indicators_single_valid_dataset(self):
+        spreadsheet = [
+            {
+                'dataset': [
+                    {
+                        'catalog_datajson_url':
+                            os.path.join(self.SAMPLES_DIR,
+                                         'single_valid_dataset.json'),
+                        'dataset_title': u'Título del dataset'
+                    }
+                ]
+            }
+        ]
+        indics = self.pad.generate_pad_indicators(spreadsheet)
+
+        expected = {
+            'pad_items_no_documentados_cant': 0,
+            'pad_items_documentados_cant': 1,
+            'pad_items_documentados_pct': 100
+        }
+        self.assertDictContainsSubset(expected, indics)
+
+    def test_documentation_indicators_single_invalid_dataset(self):
+        spreadsheet = [
+            {
+                'dataset': [
+                    {
+                        'catalog_datajson_url':
+                            os.path.join(self.SAMPLES_DIR,
+                                         'single_invalid_dataset.json'),
+                        'dataset_title': u'Título del dataset'
+                    }
+                ]
+            }
+        ]
+        indics = self.pad.generate_pad_indicators(spreadsheet)
+
+        expected = {
+            'pad_items_no_documentados_cant': 1,
+            'pad_items_documentados_cant': 0,
+            'pad_items_documentados_pct': 0
+        }
+        self.assertDictContainsSubset(expected, indics)
+
+    def test_documentation_indicators_missing_dataset_in_catalog(self):
+        spreadsheet = [
+            {
+                'dataset': [
+                    {
+                        'catalog_datajson_url':
+                            os.path.join(self.SAMPLES_DIR,
+                                         'single_valid_dataset.json'),
+                        'dataset_title': u'Dataset que no está en el .json'
+                    }
+                ]
+            }
+        ]
+        indics = self.pad.generate_pad_indicators(spreadsheet)
+
+        expected = {
+            'pad_items_no_documentados_cant': 1,
+            'pad_items_documentados_cant': 0,
+            'pad_items_documentados_pct': 0
+        }
+        self.assertDictContainsSubset(expected, indics)
+
+    def test_documentation_indicators_one_valid_one_invalid(self):
+        spreadsheet = [
+            {
+                'dataset': [
+                    {
+                        'catalog_datajson_url':
+                            os.path.join(self.SAMPLES_DIR,
+                                         'multiple_datasets.json'),
+                        'dataset_title': u'Título del dataset'
+                    },
+                    {
+                        'catalog_datajson_url':
+                            os.path.join(self.SAMPLES_DIR,
+                                         'multiple_datasets.json'),
+                        'dataset_title': u'Título del dataset 2'
+                    },
+                ]
+            }
+        ]
+        indics = self.pad.generate_pad_indicators(spreadsheet)
+
+        expected = {
+            'pad_items_no_documentados_cant': 1,
+            'pad_items_documentados_cant': 0,
+            'pad_items_documentados_pct': 0
+        }
+        self.assertDictContainsSubset(expected, indics)
+
+    def test_documentation_indicators_several_items(self):
+        spreadsheet = [
+            {
+                'dataset': [
+                    {
+                        'catalog_datajson_url':
+                            os.path.join(self.SAMPLES_DIR,
+                                         'single_valid_dataset.json'),
+                        'dataset_title': u'Título del dataset'
+                    }
+                ]
+            },
+            {
+                'dataset': [
+                    {
+                        'catalog_datajson_url':
+                            os.path.join(self.SAMPLES_DIR,
+                                         'single_valid_dataset.json'),
+                        'dataset_title': u'Dataset que no está en el .json'
+                    }
+                ]
+            }
+        ]
+        indics = self.pad.generate_pad_indicators(spreadsheet)
+
+        expected = {
+            'pad_items_no_documentados_cant': 1,
+            'pad_items_documentados_cant': 1,
+            'pad_items_documentados_pct': 50
+        }
+        self.assertDictContainsSubset(expected, indics)
+
+    def test_documentation_indicators_several_items_different_catalogs(self):
+        spreadsheet = [
+            {
+                'dataset': [
+                    {
+                        'catalog_datajson_url':
+                            os.path.join(self.SAMPLES_DIR,
+                                         'single_valid_dataset.json'),
+                        'dataset_title': u'Título del dataset'
+                    }
+                ]
+            },
+            {
+                'dataset': [
+                    {
+                        'catalog_datajson_url':
+                            os.path.join(self.SAMPLES_DIR,
+                                         'single_invalid_dataset.json'),
+                        'dataset_title': u'Título del dataset'
+                    }
+                ]
+            }
+        ]
+        indics = self.pad.generate_pad_indicators(spreadsheet)
+
+        expected = {
+            'pad_items_no_documentados_cant': 1,
+            'pad_items_documentados_cant': 1,
+            'pad_items_documentados_pct': 50
+        }
+        self.assertDictContainsSubset(expected, indics)

8ff808bc23b0b69683139a36e2873d7a97b5092c
Lucas Romero <lromero@devartis.com>
2018-04-19 11:07:27 -0300

    Revert "Validacion previa a la federacion de un catalogo e incorporacion del nodo federador"
    This reverts commit c45fa5e95f57ac1ef5a37fc9c4dc3417e9045082.

--- monitoreo/apps/dashboard/tasks.py
+++ monitoreo/apps/dashboard/tasks.py
@@ -1,37 +1,25 @@
 #! coding: utf-8
 
+from django.conf import settings
+
 from pydatajson.core import DataJson
 from pydatajson.federation import harvest_catalog_to_ckan
 from django_datajsonar.apps.management.models import Node
 from django_datajsonar.apps.api.models import Dataset
-from .models import HarvestingNode
 
 
 def harvest_run():
-    harvesting_nodes = HarvestingNode.objects.filter(enabled=True)
-    for harvester in harvesting_nodes:
-        portal_url = harvester.url
-        apikey = harvester.apikey
-        harvest_catalog(portal_url, apikey)
+    portal_url = getattr(settings, 'HARVESTING_URL', None)
+    apikey = getattr(settings, 'HARVESTING_URL_APIKEY', None)
 
-
-def harvest_catalog(portal_url, apikey):
     nodes = Node.objects.filter(indexable=True)
     for node in nodes:
         catalog_id = node.catalog_id
         catalog = DataJson(node.catalog_url)
-        dataset_list = get_dataset_list(node, catalog)
-        try:
-            harvest_catalog_to_ckan(catalog, portal_url, apikey, catalog_id, dataset_list)
-        except Exception:
-            pass
+        dataset_list = get_dataset_list(node)
+        harvest_catalog_to_ckan(catalog, portal_url, apikey, catalog_id, dataset_list)
 
 
-def get_dataset_list(node, catalog):
+def get_dataset_list(node):
     datasets = Dataset.objects.filter(catalog__identifier=node.catalog_id, indexable=True)
-    catalog_report = catalog.validate_catalog()
-    valid_datasets = set([ds['identifier'] for ds in catalog_report['error']['dataset']
-                          if ds['status'] == 'OK'])
-    return [dataset.identifier for dataset in datasets
-            if dataset.identifier in valid_datasets]
-
+    return [dataset.identifier for dataset in datasets]

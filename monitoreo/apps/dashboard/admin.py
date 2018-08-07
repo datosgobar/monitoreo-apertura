c043ae04a36338b6cb8d720c2cef54c5e08e4ca0
Lucas Romero <lromero@devartis.com>
2018-08-07 16:39:54 -0300

    Agrega botones para mandar al tope o al fondo los indicator types

--- monitoreo/apps/dashboard/admin.py
+++ monitoreo/apps/dashboard/admin.py
@@ -1,6 +1,13 @@
 # coding=utf-8
 
+from __future__ import unicode_literals
+
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.conf.urls import url
+from django.utils.html import format_html
+from django.core.urlresolvers import reverse
+
 from ordered_model.admin import OrderedModelAdmin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
@@ -55,9 +62,34 @@ class IndicatorRedAdmin(ImportExportModelAdmin):
 
 
 class IndicatorTypeAdmin(OrderedModelAdmin):
-    list_display = ('nombre', 'order', 'resumen', 'mostrar', 'move_up_down_links')
+    list_display = ('nombre', 'order', 'resumen', 'mostrar', 'move_up_down_links', 'position_actions')
     list_filter = ('resumen', 'mostrar')
     actions = ('queryset_to_top', 'queryset_to_bottom', 'summarize', 'desummarize', 'show', 'hide')
+
+    def get_urls(self):
+        urls = super(IndicatorTypeAdmin, self).get_urls()
+        extra_urls = [url(r'^(?P<model_id>.+)/(?P<direction>top|bottom)/$', self.order_move, name='order_move'), ]
+        return extra_urls + urls
+
+    def position_actions(self, obj):
+        return format_html(
+            '<a class="button" href="{}">Tope</a>&nbsp;'
+            '<a class="button" href="{}">Fondo</a>',
+            reverse('admin:order_move', args=[obj.pk, 'top']),
+            reverse('admin:order_move', args=[obj.pk, 'bottom']),
+        )
+    position_actions.short_description = 'Posicionamientos'
+    position_actions.allow_tags = True
+
+    def order_move(self, request, model_id, direction):
+        indicator_type = IndicatorType.objects.get(pk=model_id)
+        if direction == 'top':
+            indicator_type.top()
+        elif direction == 'bottom':
+            indicator_type.bottom()
+        indicator_type.save()
+
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
     summarize = switch('resumen', True)
     summarize.short_description = 'Agregar al resumen'

c2c7aa05a5c63d8e0020f22f32603814cb10be28
Lucas Romero <lromero@devartis.com>
2018-09-28 09:41:38 -0300

    tests y arreglos de errores

--- monitoreo/apps/dashboard/management/commands/delete_duplicated_indicators.py
+++ monitoreo/apps/dashboard/management/commands/delete_duplicated_indicators.py
@@ -1,16 +1,27 @@
 # coding=utf-8
 from __future__ import unicode_literals
 from django.core.management.base import BaseCommand
-from monitoreo.apps.dashboard.indicators_tasks import generate_indicators
-from monitoreo.apps.dashboard.models import IndicatorsGenerationTask
+from monitoreo.apps.dashboard.models import Indicador, IndicadorRed
+
+
+def get_duplicates(model, distinct_fields, ignore_null=False):
+    distinct = model.objects.all().distinct(*distinct_fields).values_list('pk', flat=True)
+    duplicates = model.objects.all().exclude(pk__in=distinct)
+    return duplicates
 
 
 class Command(BaseCommand):
-    help = """Calcula indicadores de la red de nodos del PAD de la república
-    Argentina. Los catálogos son automáticamente leído de su librería remota,
-    con raíz en https://www.github.com/datosgobar/libreria-catalogos"""
+    help = """Borra indicadores de red y catálogo duplicados por día. 
+    Apropos de: https://github.com/datosgobar/monitoreo-apertura/issues/109"""
+
+    def add_arguments(self, parser):
+        parser.add_argument('--ignore_null', action='store_true')
 
     def handle(self, *args, **options):
-        # Llama sincrónicamente
-        task = IndicatorsGenerationTask.objects.create()
-        generate_indicators(task)
+        duplicates = get_duplicates(IndicadorRed, ['fecha', 'indicador_tipo'])
+        duplicates.delete()
+
+        duplicates = get_duplicates(Indicador, ['fecha', 'indicador_tipo', 'jurisdiccion_nombre', 'jurisdiccion_id'])
+        if options['ignore_null']:
+            duplicates = duplicates.exclude(jurisdiccion_id=None)
+        duplicates.delete()

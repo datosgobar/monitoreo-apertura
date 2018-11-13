e5a387b45b429682e92174e1965a3666f7a16d2d
Lucas Romero <lromero@devartis.com>
2018-11-13 15:16:54 -0300

    redsitribuyo las responsabilidades entre las clases

--- monitoreo/apps/dashboard/report_tasks.py
+++ monitoreo/apps/dashboard/report_tasks.py
@@ -13,37 +13,34 @@ from django_rq import job
 from pydatajson.core import DataJson
 from django_datajsonar.models import Node
 
-from .models import Indicador, IndicadorRed, \
-    IndicatorsGenerationTask, ReportGenerationTask, ValidationReportTask
+from . import models
 
 
 @job('reports')
 def send_reports(report_task=None):
     try:
-        indicators_task = IndicatorsGenerationTask.objects\
-            .filter(status=IndicatorsGenerationTask.FINISHED).latest('finished')
-    except IndicatorsGenerationTask.DoesNotExist:
+        indicators_task = models.IndicatorsGenerationTask.objects\
+            .filter(status=models.IndicatorsGenerationTask.FINISHED).latest('finished')
+    except models.IndicatorsGenerationTask.DoesNotExist:
         # No hay un task cargado
         return
 
-    report_task = report_task or ReportGenerationTask.objects.create()
+    report_task = report_task or models.ReportGenerationTask.objects.create()
 
     generator = IndicatorReportGenerator(indicators_task, report_task)
     mail = generator.generate_email()
-    if mail:
-        generator.send_email(mail)
+    generator.send_email(mail)
 
     nodes = Node.objects.filter(indexable=True)
     for node in nodes:
         mail = generator.generate_email(node)
-        if mail:
-            generator.send_email(mail, node=node)
+        generator.send_email(mail, node=node)
     generator.close_task()
 
 
 @job('reports')
 def send_validations(validation_task=None):
-    validation_task = validation_task or ValidationReportTask.objects.create()
+    validation_task = validation_task or models.ValidationReportTask.objects.create()
     generator = ValidationReportGenerator(validation_task)
     nodes = Node.objects.filter(indexable=True)
     for node in nodes:
@@ -53,27 +50,9 @@ def send_validations(validation_task=None):
     generator.close_task()
 
 
-class AbstractReportGenerator(object):
-    DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
-    TXT_TEMPLATE = None
-    HTML_TEMPLATE = None
-
+class ReportSender(object):
     def __init__(self, report_task):
         self.report_task = report_task
-
-    def generate_email(self, node=None):
-        raise NotImplementedError
-
-    def send_email(self, mail, node=None):
-        target = node.catalog_id if node else 'Red'
-
-        try:
-            mail.send()
-            msg = "Reporte de {} enviado exitosamente".format(target)
-        except SMTPException as e:
-            msg = "Error enviando reporte de {}:\n {}".format(target, str(e))
-
-        self.report_task.info(self.report_task, msg)
 
     def _get_recipients(self, node=None):
         if not node:
@@ -82,12 +61,26 @@ class AbstractReportGenerator(object):
             recipients = node.admins.all()
         return [user.email for user in recipients if user.email]
 
-    def _render_templates(self, subject, emails, context):
-        msg = render_to_string(self.TXT_TEMPLATE, context=context)
-        html_msg = render_to_string(self.HTML_TEMPLATE, context=context)
-        mail = EmailMultiAlternatives(subject, msg, settings.EMAIL_HOST_USER, emails)
-        mail.attach_alternative(html_msg, 'text/html')
-        return mail
+    def send_email(self, mail, node=None):
+        target = node.catalog_id if node else 'Red'
+        mail.to = self._get_recipients(node=node)
+        try:
+            mail.send()
+            msg = "Reporte de {} enviado exitosamente".format(target)
+        except SMTPException as e:
+            msg = "Error enviando reporte de {}:\n {}".format(target, str(e))
+        self.report_task.info(self.report_task, msg)
+
+
+class AbstractReportGenerator(object):
+    DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+    def __init__(self, report_task):
+        self.report_task = report_task
+        self.sender = ReportSender(report_task)
+
+    def generate_email(self, node=None):
+        raise NotImplementedError
 
     def close_task(self):
         self.report_task.refresh_from_db()
@@ -97,6 +90,9 @@ class AbstractReportGenerator(object):
 
     def _format_date(self, date):
         return timezone.localtime(date).strftime(self.DATE_FORMAT)
+
+    def send_email(self, mail, node=None):
+        self.sender.send_email(mail, node=node)
 
 
 class IndicatorReportGenerator(AbstractReportGenerator):
@@ -118,26 +114,26 @@ class IndicatorReportGenerator(AbstractReportGenerator):
         }
         if not node:
             one_d_summary, multi_d_summary, _ = \
-                IndicadorRed.objects.filter(indicador_tipo__resumen=True).\
+                models.IndicadorRed.objects.filter(indicador_tipo__resumen=True).\
                 sorted_indicators_on_date(
                     self.indicators_task.finished
                     .astimezone(timezone.get_current_timezone())
                     .date())
             one_dimensional, multi_dimensional, listed = \
-                IndicadorRed.objects.filter(indicador_tipo__mostrar=True).\
+                models.IndicadorRed.objects.filter(indicador_tipo__mostrar=True).\
                 sorted_indicators_on_date(
                     self.indicators_task.finished
                     .astimezone(timezone.get_current_timezone())
                     .date())
         else:
             one_d_summary, multi_d_summary, _ = \
-                Indicador.objects.filter(indicador_tipo__resumen=True).\
+                models.Indicador.objects.filter(indicador_tipo__resumen=True).\
                 sorted_indicators_on_date(
                     self.indicators_task.finished
                     .astimezone(timezone.get_current_timezone())
                     .date(), node)
             one_dimensional, multi_dimensional, listed = \
-                Indicador.objects.filter(indicador_tipo__mostrar=True)\
+                models.Indicador.objects.filter(indicador_tipo__mostrar=True)\
                 .sorted_indicators_on_date(
                     self.indicators_task.finished
                     .astimezone(timezone.get_current_timezone())
@@ -150,13 +146,9 @@ class IndicatorReportGenerator(AbstractReportGenerator):
             'multi_dimensional_indics': multi_dimensional,
         })
 
-        emails = self._get_recipients(node=node)
-        if not emails:
-            # Nothing to do here
-            return None
         start_time = self._format_date(self.indicators_task.created)
         subject = u'[{}] Indicadores Monitoreo Apertura: {}'.format(settings.ENV_TYPE, start_time)
-        mail = self._render_templates(subject, emails, context)
+        mail = self._render_templates(subject, context)
 
         if not node:
             mail.attach('info.log', self.indicators_task.logs, 'text/plain')
@@ -166,6 +158,14 @@ class IndicatorReportGenerator(AbstractReportGenerator):
                 body = render_to_string('reports/datasets.csv', context={'dataset_list': listed[indicator]})
                 mail.attach('{}.csv'.format(indicator), body, 'text/csv')
 
+        return mail
+
+    def _render_templates(self, subject, context):
+        msg = render_to_string(self.TXT_TEMPLATE, context=context)
+        html_msg = render_to_string(self.HTML_TEMPLATE, context=context)
+        mail = EmailMultiAlternatives(subject, msg,
+                                      settings.EMAIL_HOST_USER)
+        mail.attach_alternative(html_msg, 'text/html')
         return mail
 
 
@@ -195,10 +195,14 @@ class ValidationReportGenerator(AbstractReportGenerator):
         }
 
         subject = u'[{}] Validacion de {}'.format(settings.ENV_TYPE, node.catalog_id)
-        emails = self._get_recipients(node=node)
-        if not emails:
-            # Nothing to do here
-            return None
-        mail = self._render_templates(subject, emails, context)
+        mail = self._render_templates(subject, context)
 
         return mail
+
+    def _render_templates(self, subject, context):
+        msg = render_to_string(self.TXT_TEMPLATE, context=context)
+        html_msg = render_to_string(self.HTML_TEMPLATE, context=context)
+        mail = EmailMultiAlternatives(subject, msg, settings.EMAIL_HOST_USER)
+        mail.attach_alternative(html_msg, 'text/html')
+        return mail
+

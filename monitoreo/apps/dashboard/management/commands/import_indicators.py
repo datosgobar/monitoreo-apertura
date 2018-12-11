22642052124c7d4bf3566c7eccdfe8a35704e429
Lucas Romero <lromero@devartis.com>
2018-12-11 17:24:54 -0300

    deshabilitar temporalmente auto_now_add

--- monitoreo/apps/dashboard/management/commands/import_indicators.py
+++ monitoreo/apps/dashboard/management/commands/import_indicators.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 import argparse
 
 from tablib import Dataset
+from contextlib import contextmanager
 from import_export.resources import modelresource_factory
 
 from django.core.management.base import BaseCommand
@@ -25,7 +26,28 @@ class Command(BaseCommand):
         model = IndicadorRed if options['aggregated'] else Indicador
 
         with options['file'] as indicators_csv:
-            dataset = Dataset().load(indicators_csv.read())
-            indicator_resource = modelresource_factory(model=model)()
-            result = indicator_resource.import_data(dataset)
-            print result.totals
+            with suppress_autotime(model, ['fecha']):
+                dataset = Dataset().load(indicators_csv.read())
+                indicator_resource = modelresource_factory(model=model)()
+                result = indicator_resource.import_data(dataset)
+                print result.totals
+
+
+@contextmanager
+def suppress_autotime(model, fields):
+    _original_values = {}
+    for field in model._meta.local_fields:
+        if field.name in fields:
+            _original_values[field.name] = {
+                'auto_now': field.auto_now,
+                'auto_now_add': field.auto_now_add,
+            }
+            field.auto_now = False
+            field.auto_now_add = False
+    try:
+        yield
+    finally:
+        for field in model._meta.local_fields:
+            if field.name in fields:
+                field.auto_now = _original_values[field.name]['auto_now']
+                field.auto_now_add = _original_values[field.name]['auto_now_add']

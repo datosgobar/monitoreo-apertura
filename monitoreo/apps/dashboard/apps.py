a9bfc9f8d9f516f079a6fcc84d55fa64e7ff4616
Lucas <lromero@devartis.com>
2019-02-18 12:42:57 -0300

    Cambio el nombre a la app de des

--- monitoreo/apps/dashboard/apps.py
+++ monitoreo/apps/dashboard/apps.py
@@ -1,5 +1,14 @@
+# coding=utf-8
+from __future__ import unicode_literals
+
 from django.apps import AppConfig
+from des.apps import DjangoDesConfig
 
 
 class DashboardConfig(AppConfig):
     name = 'dashboard'
+
+
+class CustomDesConfig(DjangoDesConfig):
+    verbose_name = 'Configuración correo'
+    verbose_name_plural = verbose_name

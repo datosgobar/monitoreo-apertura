69c73935e6e56e9eec354ca821809d0685002673
Lucas Romero <lromero@devartis.com>
2019-03-01 17:06:06 -0300

    reorden de apps del admin

--- monitoreo/apps/dashboard/apps.py
+++ monitoreo/apps/dashboard/apps.py
@@ -2,13 +2,7 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
-from des.apps import DjangoDesConfig
 
 
 class DashboardConfig(AppConfig):
     name = 'dashboard'
-
-
-class CustomDesConfig(DjangoDesConfig):
-    verbose_name = 'Configuración correo'
-    verbose_name_plural = verbose_name

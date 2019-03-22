3d56145962beecb3e4b2aeea30838118e89a8dc0
Rafael Romero <35603585+lrromero@users.noreply.github.com>
2019-03-22 12:08:02 -0300

    Revert "116 indicator csv column order"

--- monitoreo/apps/dashboard/helpers.py
+++ monitoreo/apps/dashboard/helpers.py
@@ -11,7 +11,7 @@ from django.http import HttpResponse
 
 from django_datajsonar.models import Dataset
 
-from .models import IndicatorsGenerationTask, IndicatorType
+from .models import IndicatorsGenerationTask
 from .strings import OVERALL_ASSESSMENT, VALIDATION_ERRORS, MISSING, HARVESTING_ERRORS, ERRORS_DIVIDER
 
 
@@ -118,12 +118,9 @@ def generate_time_series(indicators_queryset, output):
     else:
         date_range = []
 
-    ind_types = indicators_queryset\
-        .values_list('indicador_tipo__id', flat=True).distinct()
+    columns = list(set(indicators_queryset.values_list(
+        'indicador_tipo__nombre', flat=True)))
     fieldnames = ['indice_tiempo']
-    columns = IndicatorType.objects.filter(id__in=ind_types).order_by('order')\
-        .values_list('nombre', flat=True)
-    columns = list(columns)
     fieldnames = fieldnames + columns
 
     writer = csv.DictWriter(output, fieldnames, extrasaction='ignore')

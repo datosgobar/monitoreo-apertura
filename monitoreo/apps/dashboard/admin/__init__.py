8ba71770891b328d38963663d764a80b8743c928
Lucas Romero <lromero@devartis.com>
2019-03-27 16:51:06 -0300

    uso admin reorder para cambiar el nombre de los settings dinámicos

--- monitoreo/apps/dashboard/admin/__init__.py
+++ monitoreo/apps/dashboard/admin/__init__.py
@@ -1,14 +1,5 @@
 # coding=utf-8
 from __future__ import unicode_literals
 
-from django.contrib import admin
-from des.models import DynamicEmailConfiguration
-from des.admin import DynamicEmailConfigurationAdmin
-
-from monitoreo.apps.dashboard.models import ConfiguracionEmail
-
 from . import indicator_types, indicators, nodes, tasks
 
-# Proxies
-admin.site.unregister(DynamicEmailConfiguration)
-admin.site.register(ConfiguracionEmail, DynamicEmailConfigurationAdmin)

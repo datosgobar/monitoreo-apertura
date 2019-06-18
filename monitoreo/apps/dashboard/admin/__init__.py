c1c8d6dbec772807ac93d4b3f755987255f770c4
Lucas Romero <lromero@devartis.com>
2019-06-18 16:46:12 -0300

    Estilos institucionales de login

--- monitoreo/apps/dashboard/admin/__init__.py
+++ monitoreo/apps/dashboard/admin/__init__.py
@@ -1,4 +1,7 @@
 # coding=utf-8
 from __future__ import unicode_literals
+from django.contrib import admin
 
 from . import indicator_types, indicators, nodes, tasks
+
+admin.site.login_template = 'admin/login.html'

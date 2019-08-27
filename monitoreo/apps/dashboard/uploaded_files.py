cd9ca4726a28706f13f1550f82d2efd2ed08ed41
Lucas Romero <lromero@devartis.com>
2019-08-27 12:34:08 -0300

    PersistentTemporaryUploadedFile hereda de UploadedFile

--- monitoreo/apps/dashboard/uploaded_files.py
+++ monitoreo/apps/dashboard/uploaded_files.py
@@ -1,11 +1,12 @@
+import errno
 import os
 import tempfile
 
 from django.conf import settings
-from django.core.files.uploadedfile import TemporaryUploadedFile
+from django.core.files.uploadedfile import UploadedFile
 
 
-class PersistentTemporaryUploadedFile(TemporaryUploadedFile):
+class PersistentTemporaryUploadedFile(UploadedFile):
     """
     A file uploaded to a temporary location (i.e. stream-to-disk). The file does
     not delete when closed.
@@ -16,3 +17,19 @@ class PersistentTemporaryUploadedFile(TemporaryUploadedFile):
 
     def delete(self):
         return os.remove(self.file.name)
+
+    def temporary_file_path(self):
+        """
+        Returns the full path of this file.
+        """
+        return self.file.name
+
+    def close(self):
+        try:
+            return self.file.close()
+        except OSError as e:
+            if e.errno != errno.ENOENT:
+                # Means the file was moved or deleted before the tempfile
+                # could unlink it.  Still sets self.file.close_called and
+                # calls self.file.file.close() before the exception
+                raise

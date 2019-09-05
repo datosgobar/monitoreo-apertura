a7ef924c3d6392b4a6c36baa066da82ec9f04993
Franco Scaccheri Cassanello <fcassanello@devartis.com>
2019-09-05 15:19:46 -0300

    Agrego import de task

--- monitoreo/apps/dashboard/models/__init__.py
+++ monitoreo/apps/dashboard/models/__init__.py
@@ -3,4 +3,4 @@ from .indicator_types import IndicatorType, TableColumn
 from .indicators import Indicador, IndicadorRed, IndicadorFederador
 from .nodes import HarvestingNode, CentralNode
 from .tasks import ReportGenerationTask, ValidationReportTask,\
-    IndicatorsGenerationTask, FederationTask
+    IndicatorsGenerationTask, FederationTask, NewlyReportGenerationTask

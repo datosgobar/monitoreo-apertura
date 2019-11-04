363e7778e7a9c0c86c366d410a299580bb56dbbe
pepeciavirella <pep.ciavirella@gmail.com>
2019-11-04 11:20:49 -0300

    Elimino parametro no utilizado de helper para normalizacion

--- monitoreo/apps/dashboard/normalization_utils.py
+++ monitoreo/apps/dashboard/normalization_utils.py
@@ -8,7 +8,7 @@ from pydatajson.helpers import fields_to_uppercase
 LOGGER = logging.getLogger(__name__)
 
 
-def normalize_values(apps, model):
+def normalize_values(model):
     queryset = model.objects.filter(indicador_tipo__nombre__in=[
         'distribuciones_formatos_cant', 'datasets_frecuencia_cant'])
 
@@ -28,14 +28,14 @@ def normalize_values(apps, model):
 
 def normalize_node_indicator_values(apps, _schema_editor):
     indicador = apps.get_model('dashboard', 'Indicador')
-    normalize_values(apps, indicador)
+    normalize_values(indicador)
 
 
 def normalize_network_indicator_values(apps, _schema_editor):
     indicador_red = apps.get_model('dashboard', 'IndicadorRed')
-    normalize_values(apps, indicador_red)
+    normalize_values(indicador_red)
 
 
 def normalize_federator_indicator_values(apps, _schema_editor):
     indicador_federador = apps.get_model('dashboard', 'IndicadorFederador')
-    normalize_values(apps, indicador_federador)
+    normalize_values(indicador_federador)

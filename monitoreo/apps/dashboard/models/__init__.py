c1d8373e17bc6d433321468e53d2aa2ab5a8d63f
Alexis Wolfsdorf <awolfsdorf@devartis.com>
2019-11-04 14:51:18 -0300

    agregado task al init de dashboard models

--- monitoreo/apps/dashboard/models/__init__.py
+++ monitoreo/apps/dashboard/models/__init__.py
@@ -3,4 +3,5 @@ from .indicator_types import IndicatorType, TableColumn
 from .indicators import Indicador, IndicadorRed, IndicadorFederador
 from .nodes import HarvestingNode, CentralNode
 from .tasks import ReportGenerationTask, ValidationReportTask,\
-    IndicatorsGenerationTask, FederationTask, NewlyReportGenerationTask
+    IndicatorsGenerationTask, FederationTask, NewlyReportGenerationTask, \
+    NotPresentReportGenerationTask
